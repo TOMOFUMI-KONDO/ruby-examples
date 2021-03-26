694c7e7e5307da44869b29e3ce5e4ff92b8b81c5
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-26 21:47:22 +0900

    create fizz buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,1 +1,17 @@
-puts 'Hello World!'
+# frozen_string_literal: true
+
+def fizz_buzz(number)
+  if number % 15 == 0
+    'Fizz Buzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
+  end
+end
+
+(1..100).each do |i|
+  puts fizz_buzz(i)
+end
