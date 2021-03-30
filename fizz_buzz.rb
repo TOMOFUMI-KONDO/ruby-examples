6411df73ce6671274285d62a12793f1bfcb62dcd
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-30 22:17:01 +0900

    リファクタ

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 def fizz_buzz(number)
-  if number % 15 == 0
+  if (number % 15).zero?
     'Fizz Buzz'
-  elsif number % 3 == 0
+  elsif (number % 3).zero?
     'Fizz'
-  elsif number % 5 == 0
+  elsif (number % 5).zero?
     'Buzz'
   else
     number.to_s
