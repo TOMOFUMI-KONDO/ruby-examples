c1aebfff8a020299c5bd9fc026e8bf84cbaf23cf
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-04-02 08:35:30 +0900

    Refactored

--- lib/length_converter.rb
+++ lib/length_converter.rb
@@ -3,8 +3,8 @@ class LengthConverter
     @units = { m: 1.0, ft: 3.28, in: 39.37 }
   end
 
-  def convert_length(length, unit_from, unit_to)
-    (length * (@units[unit_to] / @units[unit_from])).round(2)
+  def convert_length(length, from: :m, to: :m)
+    (length * (@units[to] / @units[from])).round(2)
   end
 end
 
--- test/length_converter_test.rb
+++ test/length_converter_test.rb
@@ -7,9 +7,9 @@ class LengthConverterTest < Minitest::Test
   end
 
   def test_convert_length
-    assert_equal 39.37, @length_converter.convert_length(1, :m, :in)
-    assert_equal 1, @length_converter.convert_length(39.37, :in, :m)
-    assert_equal 0.38, @length_converter.convert_length(15, :in, :m)
-    assert_equal 10670.73, @length_converter.convert_length(35000, :ft, :m)
+    assert_equal 39.37, @length_converter.convert_length(1, from: :m, to: :in)
+    assert_equal 1, @length_converter.convert_length(39.37, from: :in, to: :m)
+    assert_equal 0.38, @length_converter.convert_length(15, from: :in, to: :m)
+    assert_equal 10670.73, @length_converter.convert_length(35000, from: :ft, to: :m)
   end
 end
