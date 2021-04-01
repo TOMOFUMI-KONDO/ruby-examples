be207a454e08ab4a1527962df690ad4d710aeff0
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-04-01 15:31:23 +0900

    Implement to_hex

--- lib/rgb_converter.rb
+++ lib/rgb_converter.rb
@@ -1,6 +1,7 @@
 class RgbConverter
   def to_hex(r, g, b)
-    ''
+    rgb = [r, g, b].map { |dec| dec.to_s(16).rjust(2, '0') }.join
+    "##{rgb}"
   end
 
   def to_ints(rgb)
--- test/test_rgb_converter.rb
+++ test/test_rgb_converter.rb
@@ -8,11 +8,11 @@ class TestRgbConverter < Minitest::Test
 
   def test_3つの10進数を16進数の文字列に変換できる
     assert_equal('#000000', @rgb_converter.to_hex(0, 0, 0))
+    assert_equal('#010101', @rgb_converter.to_hex(1, 1, 1))
+    assert_equal('#101010', @rgb_converter.to_hex(16, 16, 16))
     assert_equal('#ffffff', @rgb_converter.to_hex(255, 255, 255))
   end
 
-  def test_16進数の文字列を3つの10進数の配列に変換できる
-    assert_equal(@rgb_converter.to_ints([0, 0, 0]), '#000000')
-    assert_equal(@rgb_converter.to_ints([255, 255, 255]), '#ffffff')
-  end
+  # def test_16進数の文字列を3つの10進数の配列に変換できる
+  # end
 end
