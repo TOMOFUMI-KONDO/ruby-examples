8a37d0f2be5f6fa7e090e0c453a550e5bc1f9386
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-04-02 08:36:32 +0900

    Refactored

--- lib/length_converter.rb
+++ lib/length_converter.rb
@@ -1,10 +1,8 @@
 class LengthConverter
-  def initialize
-    @units = { m: 1.0, ft: 3.28, in: 39.37 }
-  end
+  UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
 
   def convert_length(length, from: :m, to: :m)
-    (length * (@units[to] / @units[from])).round(2)
+    (length * (UNITS[to] / UNITS[from])).round(2)
   end
 end
 
